a39cd46413c2ab47d9aae6cf7f80940d0554370c
mpperalta92 <mpperalta92@gmail.com>
2026-06-14 23:22:09 -0300

    Configuraciones para levantar el front

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,39 +1,9 @@
-import "./index.css";
-
-import Navbar from "./components/Navbar/Navbar";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import HomePage from "./pages/HomePage/HomePage";
-import ItemDetailPage from "./pages/ItemDetailPage/ItemDetailPage";
-import SuggestItemPage from "./pages/SuggestItemPage/SuggestItemPage";
-import CreateItemPage from "./pages/ItemFormPage/ItemFormPage";
-import EditItemPage from "./pages/EditItemPage/EditItemPage";
-import AdminItemsPage from "./pages/AdminItemsPage/AdminItemsPage";
-import UserProfilePage from "./pages/UserProfilePage/UserProfilePage";
-
-export function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<Layout />}>
-						<Route element={<PublicRoute />}>
-							<Route path="/" element={<HomePage />} />
-							<Route path="/auth/login" element={<LoginPage />} />
-							<Route path="/auth/register" element={<RegisterPage />} />
-							<Route path="/items/details/:id" element={<ItemDetailPage />} />
-						</Route>
-
-						<Route element={<PrivateRoute />}>
-							<Route path="/items/suggest" element={<ItemFormPage />} />
-							<Route path="/items/create" element={<ItemFormPage />} />
-							<Route path="/items/edit/:id" element={<ItemFormPage />} />
-							<Route path="/admin/items" element={<AdminItemsPage />} />
-							<Route path="/profile" element={<UserProfilePage />} />
-						</Route>{/*ItemFormPage */}
-					</Route>
-				</Routes>
-			</BrowserRouter>
-			<ToggleThemeButton />
-		</>
-	);
+function App () {
+    return (
+        <div className="App">
+<h1> HOliss</h1>
+        </div>
+    )
 }
+
+export default App
