162e43b72d8520fd01862ae65602015d1ae2a526
rodrigo zanini <super_igo@live.com.ar>
2026-06-15 22:06:46 -0300

    *actualizacion rutas

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -1,24 +1,21 @@
 import { Navigate, Outlet } from "react-router-dom";
-import  NavBar  from "./components/NavBar/NavBar";
-import  NavbarMenu  from "./components/NavBar/NavbarMenu";
 
 export function PublicRoute() {
 	const token = localStorage.getItem("token");
 	if (token) return <Navigate to="/" />;
-	return <div className="mainWrapper">
-		<Outlet />
-	</div>;
+	return (
+		<div className="mainWrapper">
+			<Outlet />
+		</div>
+	)
 }
 
 export function PrivateRoute() {
 	const token = localStorage.getItem("token");
 	if (!token) return <Navigate to="/auth/login" />;
 	return (
-		<>
-			<NavBar />
-			<div className="mainWrapper">
-				<Outlet />
-			</div>
-		</>
-	);
+		<div className="mainWrapper">
+			<Outlet />
+		</div>
+	)
 }
